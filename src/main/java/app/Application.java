8d791e38a2394ecfa64959b76869f2ed16185304
Alex_Martynov <vertikal2023@yandex.ru>
2023-11-30 11:58:43 +0400

    Условия выполнены

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -10,11 +10,10 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
-
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Animal animalCat;
+    private Animal animalDog;
+    @Autowired
+    private Timer timer;
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("cat") Animal animalCat, @Qualifier("dog") Animal animalDog ) {
+        this.animalCat = animalCat;
+        this.animalDog = animalDog;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animalCat.toString() + " , " + animalDog.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
 }
